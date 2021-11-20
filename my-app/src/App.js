a90c790eaee293d3c2c53bbb67d4523bfd5cd877
Lucas Gomez <lucasgomezmmo@gmail.com>
2021-11-20 18:23:03 -0300

    Ediciones del readme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. que onda
+          Edit <code>src/App.js</code> and save to reload. que onda que onda que onda
         </p>
         <a
           className="App-link"
