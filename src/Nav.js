a76b0c08e4636571678beefb6200b16724bc1934
LucasEzequielGomez <86528809+LucasEzequielGomez@users.noreply.github.com>
2021-12-01 12:38:59 -0300

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,7 @@
 const Nav = () => {
     return (
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#">Fixed navbar</a>
+      <a class="navbar-brand" href="#">El corralón de Bella Vista</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -11,7 +11,7 @@ const Nav = () => {
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Contacto</a>
           </li>
       </div>
     </nav>
