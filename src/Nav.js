5377a8b80e00ded514c96f94daaf66f232828416
Lucas Gomez <lucasgomezmmo@gmail.com>
2021-12-04 02:12:16 -0300

    update nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,5 @@
 const Nav = () => {
     return (
-<<<<<<< HEAD
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div class="collapse navbar-collapse" id="navbarCollapse">
         <a class="navbar-brand" href="#">El corralón de Bella Vista</a>
@@ -23,32 +22,6 @@ const Nav = () => {
           </ul>
         </div>
       </nav>
-=======
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#">El corralón de Bella Vista</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Productos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Promociones</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Presupuestos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contacto</a>
-          </li>
-      </div>
-    </nav>
->>>>>>> 7996d0c9cacaeb8e9da2e06a04617a6058b847e2
     )
 }
 
