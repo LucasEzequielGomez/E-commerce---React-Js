9767b407d2d137acf65f05eb3a5a0ec9fc33fda1
Lucas Gomez <lucasgomezmmo@gmail.com>
2021-12-04 02:08:46 -0300

    Modificaciones varias, en su mayoria en el Nav y en el Footer

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,7 @@ import Nav from "./Nav"
  const Header = () => {
     return (
             <header id="main-header">
-                <h1>E-Commerce</h1>
-                <Nav />
-                <Nav />
+            <Nav/>
             </header>
     )
  }
--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,28 @@
 const Nav = () => {
     return (
-        <nav id="main-nav" className="nav">
-            <a href="#e">Link 1</a>
-            <a href="#a">Link 2</a>
-        </nav>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+        <a class="navbar-brand" href="#">El corralón de Bella Vista</a>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Promociones</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Presupuestos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
-
 }
 
 export default Nav
