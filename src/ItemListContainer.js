c7e829bb44f17c13578f058c415588e7ea61c5d5
LucasEzequielGomez <lucasgomezmmo@gmail.com>
2021-12-15 23:07:07 -0300

    Update ItemListContainer.js

--- src/ItemListContainer.js
+++ src/ItemListContainer.js
@@ -2,12 +2,11 @@ import React from 'react'
 import ItemList from './ItemList'
 
 export const product = [
-    {id: 1, title: "barra de acero tipo Ø6", tipo: "aceros", desc: "bara de acero acindar", price: 800, talle: "aceros", img: './img/barrasDeAcero.webp', stock:1},
-    {id: 2, title: "barra de acero tipo Ø8", tipo: "aceros", desc: "bara de acero acindar", price: 850, talle: "aceros", img: './img/barrasDeAcero.webp', stock:1},
-    {id: 3, title: "barra de acero tipo Ø10", tipo: "aceros", desc: "bara de acero acindar", price: 900, talle: "aceros", img: 'img/barrasDeAcero.webp', stock:1},
-    {id: 4, title: "barra de acero tipo Ø12", tipo: "aceros", desc: "bara de acero acindar", price: 950, talle: "aceros", img: 'img/barrasDeAcero.webp', stock:1},
+    {id: 1, title: "barra de acero tipo Ø6", price: 800, img: './img/barrasDeAcero.webp', stock:5},
+    {id: 2, title: "ladrillo comun", price: 850, img: './img/barrasDeAcero.webp', stock:5},
+    {id: 3, title: "ladrillo Retak 12", price: 900, img: 'img/barrasDeAcero.webp', stock:5},
+    {id: 4, title: "cerecita de 20L", price: 950, img: 'img/barrasDeAcero.webp', stock:5},
 ]
-
 
 
 
